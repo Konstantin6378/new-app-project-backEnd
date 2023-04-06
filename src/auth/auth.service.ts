17a1b4b9fe91d7c2340c4c750237bd7f3421d97a
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-04-07 00:05:20 +0800

    add
    jwt.config and new folder Auth (auth/controller/interface/modele/service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,17 @@
+import { UserModel } from './../user/user.model';
+import { Injectable } from '@nestjs/common';
+import { ModelType } from '@typegoose/typegoose/lib/types';
+import { InjectModel } from "nestjs-typegoose";
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>
+  ) {}
+  
+  async register(dto: any){
+    const newUser = new this.UserModel({dto})
+    return newUser.save()
+  }
+
+}
