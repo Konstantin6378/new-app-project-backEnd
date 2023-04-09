7d0c82fd7a136243acba1e7ffd5997ef3ec9986f
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-04-09 18:42:15 +0800

    add
    I wrote controllers and services for the rating,
    Tested in Insomnia

---
+++ src/rating/rating.controller.ts
@@ -0,0 +1,41 @@
+import { Auth } from 'src/auth/decorators/auth.decorator'
+import { RatingService } from './rating.service'
+import {
+	Body,
+	Controller,
+	Get,
+	HttpCode,
+	Param,
+	Post,
+	UsePipes,
+	ValidationPipe,
+} from '@nestjs/common'
+import { User } from 'src/user/decorators/user.decorator'
+import { IdValidationPipe } from 'src/pipes/id.validation.pipe'
+import { Types } from 'mongoose'
+import { SetRatingDto } from './dto/set-rating.dto'
+
+@Controller('ratings')
+export class RatingController {
+	constructor(private readonly ratingService: RatingService) {}
+
+	@UsePipes(new ValidationPipe())
+	@Post('set-rating')
+	@HttpCode(200)
+	@Auth()
+	async setRating(
+		@User('_id') userId: Types.ObjectId,
+		@Body() dto: SetRatingDto
+	) {
+		return this.ratingService.setRating(userId, dto)
+	}
+
+	@Get('/:movieId')
+	@Auth()
+	async getMovieValueByUser(
+		@Param('movieId', IdValidationPipe) movieId: Types.ObjectId,
+		@User('_id') userId: Types.ObjectId
+	) {
+		return this.ratingService.getMovieValueByUser(movieId, userId)
+	}
+}
