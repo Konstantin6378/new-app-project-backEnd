8edef7331cee6c2d3570f4af9f9da1bcd3799b44
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-04-09 17:29:54 +0800

    add
    Implemented favorites,
    Tested in Insomnia

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -16,6 +16,8 @@ import {
 import { User } from './decorators/user.decorator'
 import { UpdateDto } from './dto/update.dto'
 import { IdValidationPipe } from 'src/pipes/id.validation.pipe'
+import { Types } from 'mongoose'
+import { UserModel } from './user.model'
 
 @Controller('users')
 export class UserController {
@@ -35,6 +37,23 @@ export class UserController {
 		return this.userService.updateProfile(_id, dto)
 	}
 
+	@Get('profile/favorites')
+	@Auth()
+	async getFavorites(@User('_id') _id: Types.ObjectId) {
+		return this.userService.getFavoriteMovies(_id)
+	}
+
+	// @UsePipes(new ValidationPipe())
+	@Put('profile/favorites')
+	@HttpCode(200)
+	@Auth()
+	async toggleFavorite(
+		@User() user: UserModel,
+		@Body('movieId', IdValidationPipe) movieId: Types.ObjectId
+	) {
+		return this.userService.toggleFavorite(movieId, user)
+	}
+
 	@Get('count')
 	@Auth('admin')
 	async getCountUsers() {
@@ -47,14 +66,7 @@ export class UserController {
 		return this.userService.getAll(searchTerm)
 	}
 
-	@Get(':id')
-	@Auth('admin')
-	async getUser(@Param('id', IdValidationPipe) id: string) {
-		return this.userService.byId(id)
-	}
-
 	@Delete(':id')
-	@HttpCode(200)
 	@Auth('admin')
 	async delete(@Param('id', IdValidationPipe) id: string) {
 		return this.userService.delete(id)
@@ -70,4 +82,10 @@ export class UserController {
 	) {
 		return this.userService.updateProfile(id, dto)
 	}
+
+	// @Get(':id')
+	// @Auth('admin')
+	// async getUser(@Param('id', IdValidationPipe) id: string) {
+	// 	return this.userService.byId(id)
+	// }
 }
